003eabeb5062ec09a507288af14ea01aca5ea6c1
Daniel McGiffin <danielmcgiffin@gmail.com>
2025-12-03 07:02:03 -0500

    Quartz sync: Dec 3, 2025, 7:02 AM

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "danielmcgiffin.github.io/cursus-pub-kb",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
